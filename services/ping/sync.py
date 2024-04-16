e87875886437b92c5772f30d1364fae8388a8ee5
root <root@Mayzer>
2024-04-17 00:16:07 +0300

    dependencies

--- services/ping/sync.py
+++ services/ping/sync.py
@@ -38,6 +38,7 @@ class SyncPingService(PingService):
             "Initiating ping to the server to update the activity timestamp.",
         )
         response = self._bitads_client.subnet_ping()
+        print('response', response)
         if not response.result:
             return
 
