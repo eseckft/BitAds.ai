079fffef1b94595ba5cf1ca02ebd8f887f7c1d3a
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-09-04 11:53:00 +0300

    Update setting weights

--- neurons/validator/core.py
+++ neurons/validator/core.py
@@ -186,9 +186,8 @@ class CoreValidator(BaseValidatorNeuron):
         hotkey_to_uid = {n.hotkey: n.uid for n in self.metagraph.neurons}
 
         miner_ratings = {
-            hotkey_to_uid[hotkey]: rating
-            for hotkey, rating in self.miner_ratings.items()
-            if hotkey in hotkey_to_uid
+            uid: self.miner_ratings.get(hotkey, 0.0)
+            for hotkey, uid in hotkey_to_uid.items()
         }
         bt.logging.debug(f"UID to rating: {miner_ratings}")
 
@@ -197,8 +196,7 @@ class CoreValidator(BaseValidatorNeuron):
             netuid=self.config.netuid,
             uids=list(miner_ratings.keys()),
             weights=list(miner_ratings.values()),
-            wait_for_finalization=False,
-            wait_for_inclusion=False,
+            wait_for_finalization=True,
         )
         if result is True:
             bt.logging.info("set_weights on chain successfully!")
