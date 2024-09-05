3a083fe7b733de1e161dd44a1ec4383ec848a9f8
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-09-05 20:45:04 +0300

    Temporary fix

--- neurons/validator/core.py
+++ neurons/validator/core.py
@@ -140,8 +140,8 @@ class CoreValidator(BaseValidatorNeuron):
         bt.logging.info("Start sync bitads process")
         offset = await self.bitads_service.get_last_update_bitads_data(
             self.wallet.get_hotkey().ss58_address
-        ) - timedelta(days=5)  # Temporary measure for validator sync
-        bt.logging.debug(f"Sync visits with offset: {offset}")
+        )
+        bt.logging.debug(f"Sync visits with offset: {offset} with miners: {self.miners}")
         responses = await forward_each_axon(
             self,
             SyncVisits(offset=offset),
