7320e71993854872e0a35c82d1112ccbe73ccdcc
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-09-06 01:54:55 +0300

    Try reboot validator

--- proxies/validator.py
+++ proxies/validator.py
@@ -70,7 +70,7 @@ async def lifespan(app: FastAPI):
     subtensor.close()
 
 
-app = FastAPI(version="0.2.7", lifespan=lifespan)
+app = FastAPI(version="0.2.8", lifespan=lifespan)
 
 app.include_router(version_router)
 app.include_router(test_router)
