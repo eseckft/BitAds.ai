74eeda3e6db7a586666c19400f387e7da4e450b2
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-10-29 14:45:19 +0200

    Comment all scores

--- template/base/validator.py
+++ template/base/validator.py
@@ -218,55 +218,55 @@ class BaseValidatorNeuron(BaseNeuron):
         """
 
         # Check if self.scores contains any NaN values and log a warning if it does.
-        if torch.isnan(self.scores).any():
-            bt.logging.warning(
-                f"Scores contain NaN values. This may be due to a lack of responses from miners, or a bug in your reward functions."
-            )
-
-        # Calculate the average reward for each uid across non-zero values.
-        # Replace any NaN values with 0.
-        raw_weights = torch.nn.functional.normalize(self.scores, p=1, dim=0)
-
-        bt.logging.debug("raw_weights", raw_weights)
-        bt.logging.debug("raw_weight_uids", self.metagraph.uids.to("cpu"))
-        # Process the raw weights to final_weights via subtensor limitations.
-        (
-            processed_weight_uids,
-            processed_weights,
-        ) = bt.utils.weight_utils.process_weights_for_netuid(
-            uids=self.metagraph.uids.to("cpu"),
-            weights=raw_weights.to("cpu"),
-            netuid=self.config.netuid,
-            subtensor=self.subtensor,
-            metagraph=self.metagraph,
-        )
-        bt.logging.debug("processed_weights", processed_weights)
-        bt.logging.debug("processed_weight_uids", processed_weight_uids)
-
-        # Convert to uint16 weights and uids.
-        (
-            uint_uids,
-            uint_weights,
-        ) = bt.utils.weight_utils.convert_weights_and_uids_for_emit(
-            uids=processed_weight_uids, weights=processed_weights
-        )
-        bt.logging.debug("uint_weights", uint_weights)
-        bt.logging.debug("uint_uids", uint_uids)
-
-        # Set the weights on chain via our subtensor connection.
-        result, msg = self.subtensor.set_weights(
-            wallet=self.wallet,
-            netuid=self.config.netuid,
-            uids=uint_uids,
-            weights=uint_weights,
-            wait_for_finalization=False,
-            wait_for_inclusion=False,
-            version_key=self.spec_version,
-        )
-        if result is True:
-            bt.logging.info("set_weights on chain successfully!")
-        else:
-            bt.logging.error("set_weights failed", msg)
+        # if torch.isnan(self.scores).any():
+        #     bt.logging.warning(
+        #         f"Scores contain NaN values. This may be due to a lack of responses from miners, or a bug in your reward functions."
+        #     )
+        #
+        # # Calculate the average reward for each uid across non-zero values.
+        # # Replace any NaN values with 0.
+        # raw_weights = torch.nn.functional.normalize(self.scores, p=1, dim=0)
+        #
+        # bt.logging.debug("raw_weights", raw_weights)
+        # bt.logging.debug("raw_weight_uids", self.metagraph.uids.to("cpu"))
+        # # Process the raw weights to final_weights via subtensor limitations.
+        # (
+        #     processed_weight_uids,
+        #     processed_weights,
+        # ) = bt.utils.weight_utils.process_weights_for_netuid(
+        #     uids=self.metagraph.uids.to("cpu"),
+        #     weights=raw_weights.to("cpu"),
+        #     netuid=self.config.netuid,
+        #     subtensor=self.subtensor,
+        #     metagraph=self.metagraph,
+        # )
+        # bt.logging.debug("processed_weights", processed_weights)
+        # bt.logging.debug("processed_weight_uids", processed_weight_uids)
+        #
+        # # Convert to uint16 weights and uids.
+        # (
+        #     uint_uids,
+        #     uint_weights,
+        # ) = bt.utils.weight_utils.convert_weights_and_uids_for_emit(
+        #     uids=processed_weight_uids, weights=processed_weights
+        # )
+        # bt.logging.debug("uint_weights", uint_weights)
+        # bt.logging.debug("uint_uids", uint_uids)
+        #
+        # # Set the weights on chain via our subtensor connection.
+        # result, msg = self.subtensor.set_weights(
+        #     wallet=self.wallet,
+        #     netuid=self.config.netuid,
+        #     uids=uint_uids,
+        #     weights=uint_weights,
+        #     wait_for_finalization=False,
+        #     wait_for_inclusion=False,
+        #     version_key=self.spec_version,
+        # )
+        # if result is True:
+        #     bt.logging.info("set_weights on chain successfully!")
+        # else:
+        #     bt.logging.error("set_weights failed", msg)
 
     def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
@@ -286,20 +286,20 @@ class BaseValidatorNeuron(BaseNeuron):
             "Metagraph updated, re-syncing hotkeys, dendrite pool and moving averages"
         )
         # Zero out all hotkeys that have been replaced.
-        for uid, hotkey in enumerate(self.hotkeys):
-            if hotkey != self.metagraph.hotkeys[uid]:
-                self.scores[uid] = 0  # hotkey has been replaced
+        # for uid, hotkey in enumerate(self.hotkeys):
+        #     if hotkey != self.metagraph.hotkeys[uid]:
+        #         self.scores[uid] = 0  # hotkey has been replaced
 
         # Check to see if the metagraph has changed size.
         # If so, we need to add new hotkeys and moving averages.
-        if len(self.hotkeys) < len(self.metagraph.hotkeys):
-            # Update the size of the moving average scores.
-            new_moving_average = torch.zeros((self.metagraph.n)).to(
-                self.device
-            )
-            min_len = min(len(self.hotkeys), len(self.scores))
-            new_moving_average[:min_len] = self.scores[:min_len]
-            self.scores = new_moving_average
+        # if len(self.hotkeys) < len(self.metagraph.hotkeys):
+        #     # Update the size of the moving average scores.
+        #     new_moving_average = torch.zeros((self.metagraph.n)).to(
+        #         self.device
+        #     )
+        #     min_len = min(len(self.hotkeys), len(self.scores))
+        #     new_moving_average[:min_len] = self.scores[:min_len]
+        #     self.scores = new_moving_average
 
         # Update the hotkeys.
         self.hotkeys = copy.deepcopy(self.metagraph.hotkeys)
@@ -308,31 +308,31 @@ class BaseValidatorNeuron(BaseNeuron):
         """Performs exponential moving average on the scores based on the rewards received from the miners."""
 
         # Check if rewards contains NaN values.
-        if torch.isnan(rewards).any():
-            bt.logging.warning(f"NaN values detected in rewards: {rewards}")
-            # Replace any NaN values in rewards with 0.
-            rewards = torch.nan_to_num(rewards, 0)
-
-        # Check if `uids` is already a tensor and clone it to avoid the warning.
-        if isinstance(uids, torch.Tensor):
-            uids_tensor = uids.clone().detach()
-        else:
-            uids_tensor = torch.tensor(uids).to(self.device)
-
-        # Compute forward pass rewards, assumes uids are mutually exclusive.
-        # shape: [ metagraph.n ]
-        scattered_rewards: torch.FloatTensor = self.scores.scatter(
-            0, uids_tensor, rewards
-        ).to(self.device)
-        bt.logging.debug(f"Scattered rewards: {rewards}")
-
-        # Update scores with rewards produced by this step.
-        # shape: [ metagraph.n ]
-        alpha: float = self.config.neuron.moving_average_alpha
-        self.scores: torch.FloatTensor = alpha * scattered_rewards + (
-            1 - alpha
-        ) * self.scores.to(self.device)
-        bt.logging.debug(f"Updated moving avg scores: {self.scores}")
+        # if torch.isnan(rewards).any():
+        #     bt.logging.warning(f"NaN values detected in rewards: {rewards}")
+        #     # Replace any NaN values in rewards with 0.
+        #     rewards = torch.nan_to_num(rewards, 0)
+        #
+        # # Check if `uids` is already a tensor and clone it to avoid the warning.
+        # if isinstance(uids, torch.Tensor):
+        #     uids_tensor = uids.clone().detach()
+        # else:
+        #     uids_tensor = torch.tensor(uids).to(self.device)
+        #
+        # # Compute forward pass rewards, assumes uids are mutually exclusive.
+        # # shape: [ metagraph.n ]
+        # scattered_rewards: torch.FloatTensor = self.scores.scatter(
+        #     0, uids_tensor, rewards
+        # ).to(self.device)
+        # bt.logging.debug(f"Scattered rewards: {rewards}")
+        #
+        # # Update scores with rewards produced by this step.
+        # # shape: [ metagraph.n ]
+        # alpha: float = self.config.neuron.moving_average_alpha
+        # self.scores: torch.FloatTensor = alpha * scattered_rewards + (
+        #     1 - alpha
+        # ) * self.scores.to(self.device)
+        # bt.logging.debug(f"Updated moving avg scores: {self.scores}")
 
     def save_state(self):
         """Saves the state of the validator to a file."""
