f51962f93ee0b7fb6e9702c087e1ff19646d7a4d
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-11-18 17:16:37 +0200

    WIP

--- common/environ.py
+++ common/environ.py
@@ -5,26 +5,18 @@ from os import environ
 class Environ:
     """
     Configuration class for environment variables used in the Validator application.
-
-    Attributes:
-        MAIN_DB_URL (str): URL for the main database. Defaults to 'sqlite+aiosqlite:///main.db'.
-        GEO2_LITE_DB_PATH (str): File path for GeoLite2 country database. Defaults to 'GeoLite2-Country.mmdb'.
-        DB_URL_TEMPLATE (str): Template string for generating database URLs based on name and network.
-                               Defaults to 'sqlite:///{name}_{network}.db'.
-        SUBTENSOR_NETWORK (str): Identifier for the Subtensor network. Defaults to 'finney'.
-        WALLET_NAME (str, optional): Name or identifier of the wallet. Defaults to None.
-        WALLET_HOTKEY (str, optional): Hotkey address associated with the wallet. Defaults to None.
-        VALIDATORS (list): List of validators parsed from environment variables as a JSON array.
-                           Defaults to an empty list ([]).
-        MINERS (list): List of miners parsed from environment variables as a JSON array.
-                       Defaults to an empty list ([]).
     """
-    MAIN_DB_URL: str = environ.get("MAIN_DB_URL", "sqlite+aiosqlite:///main.db")
+
     GEO2_LITE_DB_PATH: str = environ.get("GEO2_LITE_DB_PATH", "GeoLite2-Country.mmdb")
-    DB_URL_TEMPLATE: str = environ.get("DB_URL_TEMPLATE", "sqlite:///{name}_{network}.db")
-    SUBTENSOR_NETWORK: str = environ.get("SUBTENSOR_NETWORK", "finney")
-    WALLET_NAME: str = environ.get("WALLET_NAME")
-    WALLET_HOTKEY: str = environ.get("WALLET_HOTKEY")
+    DB_URL_TEMPLATE: str = environ.get(
+        "DB_URL_TEMPLATE", "sqlite:///{name}_{network}.db"
+    )
+    WALLET_NAME: str = environ.get("WALLET_NAME", "default")
+    HOTKEY_NAME: str = environ.get("HOTKEY_NAME", "default")
+    NETUID: int = int(environ["NETUID"])
+    SUBTENSOR_NETWORK: str = environ.get("SUBTENSOR_NETWORK")
+    SUBTENSOR_ADDRESS: str = environ.get("SUBTENSOR_ADDRESS")
     VALIDATORS: list = json.loads(environ.get("VALIDATORS", "[]"))
     MINERS: list = json.loads(environ.get("MINERS", "[]"))
-    NEURON_TYPE = environ.get("NEURON_TYPE", "neuron")
+    NEURON_TYPE: str = environ.get("NEURON_TYPE", "neuron")
+    PROXY_PORT: int = int(environ.get("PROXY_PORT", 443))
--- neurons/miner/lifespan.py
+++ neurons/miner/lifespan.py
@@ -5,6 +5,7 @@ import threading
 from collections.abc import AsyncIterator, Callable, Sequence
 from contextlib import AbstractAsyncContextManager
 
+from common.environ import Environ
 from fastapi import FastAPI
 from fiber.chain import interface, chain_utils, post_ip_to_chain
 from fiber.logging_utils import get_logger
@@ -79,38 +80,9 @@ def get_external_ip():
     return ip
 
 
-# NOTE this is also a script in /scropts/post_ip_to_chain, and you can use it on the cli with fiber-post-ip
-def post_miner_ip_to_chain():
-    chain_endpoint = None
-    subtensor_network = "finney"
-    wallet_name = "prod_miner"
-    wallet_hotkey = "prod_miner"
-    netuid = 16
-    external_port = 443
-
-    substrate = interface.get_substrate(
-        subtensor_address=chain_endpoint, subtensor_network=subtensor_network
-    )
-    keypair = chain_utils.load_hotkey_keypair(
-        wallet_name=wallet_name, hotkey_name=wallet_hotkey
-    )
-    coldkey_keypair_pub = chain_utils.load_coldkeypub_keypair(wallet_name=wallet_name)
-
-    success = post_ip_to_chain.post_node_ip_to_chain(
-        substrate=substrate,
-        keypair=keypair,
-        netuid=netuid,
-        external_ip=get_external_ip(),
-        external_port=external_port,
-        coldkey_ss58_address=coldkey_keypair_pub.ss58_address,
-    )
-    logger.info(f"Post IP to chain: {success}!")
-
-
 # noinspection PyAsyncCall
 @contextlib.asynccontextmanager
 async def bitads_lifespan(app: FastAPI) -> AsyncIterator[None]:
-    post_miner_ip_to_chain()
     asyncio.create_task(infinite_task())  # Start the background task
     yield
 
