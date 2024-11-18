5d3ac8cdf3f53c500728beb88678222f5af9e195
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-11-18 17:53:15 +0200

    WIP

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+
+  ],
+  deploy: {
+    local: {
+      host: 'localhost',
+      ref: 'origin/main',
+      repo: 'https://github.com/eseckft/BitAds.ai.git',
+      path: 'BitAds.ai',
+      'pre-setup': 'echo "This is a pre-setup command"',
+      'pre-deploy': 'echo "This is a pre-deploy command"',
+    },
+  },
+};
