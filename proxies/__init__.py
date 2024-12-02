83abc406bec4b8571d35dddd36c2e261a27ac6c8
k.panchenko <dev.yellowduckcoders@gmail.com>
2024-12-02 15:51:54 +0200

    Fix sorting

--- proxies/__init__.py
+++ proxies/__init__.py
@@ -1,2 +1,2 @@
-__validator_version__ = "0.6.6"
+__validator_version__ = "0.6.7"
 __miner_version__ = "0.6.3"
--- proxies/validator.py
+++ proxies/validator.py
@@ -96,7 +96,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(
-    version="0.6.6",
+    version="0.6.7",
     lifespan=lifespan,
     debug=True,
     docs_url=None,
