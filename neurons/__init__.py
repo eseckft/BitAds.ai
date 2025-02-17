6618b58a096f71377d8308e0f1b060ba703d7014
k.panchenko <dev.yellowduckcoders@gmail.com>
2025-02-17 21:08:45 +0100

    Increment for restart

--- neurons/__init__.py
+++ neurons/__init__.py
@@ -2,7 +2,7 @@
 # Define the version of the template module.
 
 
-__version__ = "3.7.7"
+__version__ = "3.9.0"
 
 
 version_split = __version__.split(".")
@@ -11,3 +11,6 @@ __spec_version__ = (
     + (10000 * int(version_split[1]))
     + (1000 * int(version_split[2]))
 )
+
+
+print(__spec_version__)
